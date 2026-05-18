906d616eb876fe446c456e4557af7ec3d88d9d62
ahemmm <akifm1373@gmail.com>
2026-05-18 22:23:36 +0600

    Fix import path for uuid package

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { v4 as uuidv4 } from 'url';
+import { v4 as uuidv4 } from 'uuid';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import logger from './utils/logger.js';
